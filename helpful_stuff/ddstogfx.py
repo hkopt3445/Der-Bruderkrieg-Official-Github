b2699313715e61936aec6b00d92244245e137b1b
arj909 <asmaitra2006@gmail.com>
2020-12-14 18:27:20 -0600

    some more icons and restructuring for austrias tree

--- helpful_stuff/ddstogfx.py
+++ helpful_stuff/ddstogfx.py
@@ -1,13 +1,13 @@
 import os
  
-directory = r"C:\Users\asen9\OneDrive\Documents\Paradox Interactive\Hearts of Iron IV\mod\DBK_backup_stuff\gfx\interface\aaa_manage_bavaria_gui"
+directory = r"C:\Users\asen9\OneDrive\Documents\Paradox Interactive\Hearts of Iron IV\mod\DBK_backup_stuff\gfx\interface\aaa_westfalen_gui"
 
 for filename in os.listdir(directory):
    filenamestorage = filename[:-4]
    if filename.endswith(".dds"):
     print("spriteType = {")
     print("name = GFX_bavarian_management" + filenamestorage) 
-    print("texturefile = " + "gfx/interface/aaa_manage_bavaria_gui/" + filename )
+    print("texturefile = " + "gfx/interface/aaa_westfalen_gui/" + filename )
     print("}")
     print("")
    else:
